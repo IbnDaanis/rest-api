8a5512d1a3d5812795afa7b659792f74333cd2f2
Ikraam <ibndaanis@gmail.com>
2021-10-09 19:32:18 +0100

    create user schema

--- src/middleware/validateResource.ts
+++ src/middleware/validateResource.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import { AnyZodObject } from 'zod'
 
-export const validate =
+export const validateResource =
   (schema: AnyZodObject) => (req: Request, res: Response, next: NextFunction) => {
     try {
       schema.parse({
@@ -9,6 +9,8 @@ export const validate =
         query: req.query,
         params: req.params
       })
+
+      next()
     } catch (error: any) {
       return res.status(400).send(error.errors)
     }
